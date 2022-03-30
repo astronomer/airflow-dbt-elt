19a3e8cb95f6eb4b36722f11e04f7dbd1547a0dd
Josh Fell <josh.d.fell@astronomer.io>
2022-03-30 08:18:25 -0400

    Update customers model

--- dbt/models/customers.sql
+++ dbt/models/customers.sql
@@ -24,7 +24,7 @@ customer_orders as (
         min(order_date) as first_order,
         max(order_date) as most_recent_order,
         count(order_id) as number_of_orders
-    from all_orders
+    from orders
 
     group by 1
 
